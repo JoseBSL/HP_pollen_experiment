bfba95d4af35c5d579ad8b0242302c96ea9babe3
JoseBSL <barragansljose@gmail.com>
2018-02-16 14:09:13 +1100

    First commit with seed set data

---
+++ seed_set_plots.R
@@ -0,0 +1,17 @@
+
+#In this script I'm going to represent the seed set of the different species 
+
+#First species Petunia integrifolia, CODE=PEIN, N=10 for each treatment, just represented the ones that we have obtained seed set
+
+
+library(reshape2)
+
+
+seed_set <- read.csv("PEIN_seed_set.csv", sep=";")
+
+seed_set <- dcast(Treatment ~ ., value.var = "Seed.production", fun.aggregate = mean, data = seed_set, na.rm= TRUE)
+
+plot(seed_set)
+
+
+
