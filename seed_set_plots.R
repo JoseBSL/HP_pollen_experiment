8fcfe40da1bf3194bc7309c3c7be624fd063fc2a
JoseBSL <barragansljose@gmail.com>
2018-02-17 15:52:49 +1100

    plot with error bars

--- seed_set_plots.R
+++ seed_set_plots.R
@@ -8,11 +8,27 @@ library(reshape2)
 
 seed_set <- read.csv("PEIN_seed_set.csv", sep=";")
 
-seed_set <- dcast(Treatment ~ ., value.var = "Seed.production", fun.aggregate = mean, data = seed_set, na.rm= TRUE)
+mean_seed_seet <- dcast(Treatment ~ ., value.var = "Seed.production", fun.aggregate = mean, data = seed_set, na.rm= TRUE)
+
+mean_seed_seet$.-> avg
+
+sd_seed_seet <- dcast(Treatment ~ ., value.var = "Seed.production", fun.aggregate = sd, data = seed_set, na.rm= TRUE)
+
+sd_seed_seet$. -> sdev
+
+sdev[4] <- 0
 
 plot(seed_set, cex.axis=0.5)
 
+x <- 1:7
 
+plot(x, avg,
+     ylim=range(c(avg-sdev, avg+sdev)),
+     pch=19, xlab="Measurements", ylab="Mean +/- SD",
+     main="Scatter plot with std.dev error bars"
+)
+# hack: we draw arrows but with very special "arrowheads"
+arrows(x, avg-sdev, x, avg+sdev, length=0.05, angle=90, code=3)
 
 
 
