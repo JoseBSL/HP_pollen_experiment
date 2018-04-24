6b1227f18d2040ddcd69baf9118e316f42f64d0a
JoseBSL <barragansljose@gmail.com>
2018-04-24 16:19:28 +1000

    some edits to the boxplot

--- ovules.R
+++ ovules.R
@@ -6,9 +6,14 @@ library(forcats)
 #load data
 d<-read.csv("Ovules.csv", sep=";")
 
+colnames(d)[1]<-"C. annuum"
+colnames(d)[2]<-"S. lycopersicum"
+colnames(d)[3]<-"S. melongera"
+colnames(d)[4]<-"P. integrifolia"
+
+
 #Reorganise data
 d <- stack(d)
-
 #Naming columns
 colnames(d)[1]<-"ovules"
 colnames(d)[2]<-"species"
@@ -25,8 +30,13 @@ solanaceae<-subset(d)[1:60,]
 
 ggplot(solanaceae, aes(x = species, y = ovules)) + geom_boxplot()
 
-ggplot(solanaceae, aes(x = reorder(species, ovules, FUN = median), y = ovules)) + geom_boxplot()
+ggplot(solanaceae, aes(x = reorder(species, ovules, FUN = median), y = ovules)) +   geom_boxplot(fill='#A4A4A4', color="black")+
+  theme_classic()
 
+p <- ggplot(solanaceae, aes(x = reorder(species, ovules, FUN = median), y = ovules)) +   geom_boxplot()+
+labs(title="Solanaceae",x="", y = "Ovules")+
+theme(plot.title = element_text(hjust = 0.5))
 
+p + stat_summary(fun.y=mean, geom="point", shape="*", size=5, colour="black")
 
 
