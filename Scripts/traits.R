ddb5409d242d096ad3706b45ab7cec19a68e746c
JoseBSL <barragansljose@gmail.com>
2018-06-08 12:49:28 +1000

    trait table

--- Scripts/traits.R
+++ Scripts/traits.R
@@ -11,17 +11,33 @@ brassicaceae <- c("Brassica oleracea", "Brassica rapa" ,"Eruca sativa", "Sinapis
 bra_fam <- rep("Brassicaceae",4)
 bra_genus <- c("Brassica", "Brassica" ,"Eruca", "Sinapis")
 
-
+#Convolvulaceae
 convolvulaceae <- c("Ipomoea aquatica", "Ipomoea purpurea")
 con_fam <- c("Convolvulaceae","Convolvulaceae")
 con_genus <- c("Ipomoea", "Ipomoea")
 
-
+#linking vectors
 species <- c(solanaceae,brassicaceae, convolvulaceae)
 genus <- c(sol_genus,bra_genus,con_genus)
 family <- c(sol_fam,bra_fam,con_fam)
-species<- data.frame(species)
-
-
+stigma_type <- rep("",10)
+compatibility_system <- rep("",10)
 tab <- cbind(family,genus,species)
 View(tab)
+
+#Adding stigma type column 
+tab<- cbind(tab,stigma_type, compatibility_system)
+tab<- data.frame(tab, stringsAsFactors=FALSE)
+class(tab)
+
+#Petunia Integrifolia stigma
+tab[1,4]<- "wet"
+#Capsicum anuum
+tab[2,4]<- "wet"
+#Solanum melongera
+tab[4,4]<- "wet"
+
+#Brassicas and convolvulaceas
+tab[5:10,4]<- "dry"
+
+
