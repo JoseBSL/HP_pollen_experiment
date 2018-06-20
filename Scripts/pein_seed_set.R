b831ee7a95db8ff06e3c35413ee125db845b2b4e
JoseBSL <barragansljose@gmail.com>
2018-06-21 01:51:09 +1000

    adding SOME average plus sd plot

--- Scripts/pein_seed_set.R
+++ Scripts/pein_seed_set.R
@@ -34,7 +34,7 @@ text(x, par("usr")[3] - 0.2, labels = lablist.x, adj = 1.25,srt = 45, xpd = TRUE
 #Unify mean and sd with merge
 mean_sd <- merge(mean_seed_seet,sd_seed_seet, by="Treatment")
 #Order from lower to higher values the average, to plot it nicely
-mean_sd_ordered <- mean_sd[order(avg),] 
+mean_sd_ordered <- mean_sd[order(mean_sd$avg),] 
 
 x <- 1:24
 plot(x, mean_sd_ordered$avg,
@@ -49,4 +49,26 @@ text(x, par("usr")[3] - 0.2, labels = lablist.x, adj = 1.25,srt = 45, xpd = TRUE
 
 #Now with eggplant (S. melongena, variety "little fingers")
 #SOME
+
+some_seed_set <- read.csv("Data/species_seed_set/SOME_seed_set.csv", sep=";")
+mean_seed_seet_some <- dcast(Treatment ~ ., value.var = "seed_set", fun.aggregate = mean, data = some_seed_set, na.rm= TRUE)
+colnames(mean_seed_seet_some)[2] <-"avg"
+sd_seed_seet_some <- dcast(Treatment ~ ., value.var = "seed_set", fun.aggregate = sd, data = some_seed_set, na.rm= TRUE)
+colnames(sd_seed_seet_some)[2] <-"sdev"
+
+#Unify mean and sd with merge
+mean_sd_some <- merge(mean_seed_seet_some,sd_seed_seet_some, by="Treatment")
+#Order from lower to higher values the average, to plot it nicely
+mean_sd_ordered_some <- mean_sd_some[order(mean_sd_some$avg),] 
+
+x <- 1:23
+plot(x, mean_sd_ordered_some$avg,
+     ylim=range(c(mean_sd_ordered_some$avg-mean_sd_ordered_some$sdev, mean_sd_ordered_some$avg+mean_sd_ordered_some$sdev)),
+     pch=19, xlab="", xaxt='n', ylab="Mean +/- SD",
+     main="Scatter plot with std.dev error bars")
+# add arrows
+arrows(x, mean_sd_ordered_some$avg-mean_sd_ordered_some$sdev, x, mean_sd_ordered_some$avg+mean_sd_ordered_some$sdev, length=0.05, angle=90, code=3)
+lablist.x<-as.vector(mean_sd_ordered_some$Treatment)
+axis(x, at=1:24, labels = FALSE)
+text(x, par("usr")[3] - 0.2, labels = lablist.x, adj = 1.25,srt = 45, xpd = TRUE)
 
