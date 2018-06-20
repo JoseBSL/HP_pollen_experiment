ffb06f643110fdb9e197af8677964200da3393ac
JoseBSL <barragansljose@gmail.com>
2018-06-20 15:27:02 +1000

    rbcl distance

---
+++ Scripts/phylogenetic_distances.R
@@ -0,0 +1,10 @@
+
+#Chloroplast matrix with phylogenetic distances (RBCL)
+
+all_species_rbcl<-read.csv("data/species_matrix.csv", sep = ";", header = F)
+View(all_species_rbcl)
+species<- all_species_rbcl$V1
+rownames(all_species_rbcl) <- species
+all_species_rbcl<- all_species_rbcl[,-1]
+colnames(all_species_rbcl) <- species
+
