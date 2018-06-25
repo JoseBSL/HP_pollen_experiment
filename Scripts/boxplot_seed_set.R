bab8e0da97b41c1676c33efe44f3300974bddd80
JoseBSL <barragansljose@gmail.com>
2018-06-25 23:51:52 +1000

    fixing some mistakes

--- Scripts/boxplot_seed_set.R
+++ Scripts/boxplot_seed_set.R
@@ -4,13 +4,14 @@
 #load libraries
 library(reshape2)
 library(ggplot2)
+library(dplyr)
 
 
 
 #read data and cleaning
 pein_seed_set <- read.csv("Data/species_seed_set/PEIN_seed_set.csv", sep=";")
 pein_seed_set <- pein_seed_set[-c(42,47,49),]
-some_seed_set <- read.csv("Data/species_seed_set/SOME_seed_set.csv", sep=";")
+caan_seed_set <- read.csv("Data/species_seed_set/CAAN_seed_set.csv", sep=";")
 
 #
 ##
@@ -22,7 +23,7 @@ some_seed_set <- read.csv("Data/species_seed_set/SOME_seed_set.csv", sep=";")
 
 
 #Removing RARA, species not considered because sterility 
-library(dplyr)
+
 pein_seed_set <- filter(pein_seed_set, Treatment!="RARA 100%" & Treatment!="RARA 50%")
 
 #adding the focal species, first 50%
@@ -30,17 +31,18 @@ Species <- rep("PEIN", 10)
 Treatment <- rep("PEIN 50%", 10)
 Treatment.number <- seq(1:10)
 Seed.production <- rep(0, 10)
-pein_50 <- data.frame(Species,Treatment, Treatment.number, Seed.production)
+pein_50 <- data.frame(Species,Treatment, Treatment.number, Seed.production,stringsAsFactors = F)
 #adding the focal species, 100%
 Species <- rep("PEIN", 10)
 Treatment <- rep("PEIN 100%", 10)
 Treatment.number <- seq(1:10)
 Seed.production <- rep(0, 10)
-pein_100<-  data.frame(Species,Treatment, Treatment.number, Seed.production)
+pein_100<-  data.frame(Species,Treatment, Treatment.number, Seed.production,stringsAsFactors = F)
 #Order from lower to higher values the average, to plot it nicely
 pein_seed_set <- rbind(pein_seed_set, pein_50, pein_100)
-
+pein_seed_set$Treatment<- as.character(pein_seed_set$Treatment)
 pein_seed_set <- pein_seed_set[order(pein_seed_set$Treatment),]  
+
 
 
 p <- ggplot(pein_seed_set, aes(x = Treatment, y = Seed.production)) +   geom_boxplot()+
@@ -58,7 +60,7 @@ p + stat_summary(fun.y=mean, geom="point", shape="*", size=5, colour="black") +t
 #
 
 #Taking out species that weren't used
-
+some_seed_set <- read.csv("Data/species_seed_set/SOME_seed_set.csv", sep=";")
 some_seed_set <- filter(some_seed_set, Treatment!="RARA 100%" & Treatment!="RARA 50%" & Treatment!="COSA 50%" & Treatment!="COSA 100%")
 
 #adding the focal species, first 50%
@@ -74,10 +76,11 @@ Treatment.number <- seq(1:10)
 Seed.production <- rep(0, 10)
 some_100<-  data.frame(Species,Treatment, Treatment.number, Seed.production)
 #Order from lower to higher values the average, to plot it nicely
+some_seed_set<- some_seed_set[,-3]
+colnames(some_seed_set)[3] <- "Treatment.number"
+colnames(some_seed_set)[4] <- "Seed.production"
 some_seed_set <- rbind(some_seed_set, some_50, some_100)
 some_seed_set <- some_seed_set[order(some_seed_set$Treatment),]  
-
-colnames(some_seed_set)[5] <- "Seed.production"
 
 
 p <- ggplot(some_seed_set, aes(x = Treatment, y = Seed.production)) +   geom_boxplot()+
@@ -94,5 +97,7 @@ p + stat_summary(fun.y=mean, geom="point", shape="*", size=5, colour="black") +t
 ###
 ##
 #
+
+caan_seed_set <- filter(caan_seed_set, Treatment!="RARA 100%" & Treatment!="RARA 50%" & Treatment!="COSA 50%" & Treatment!="COSA 100%")
 
 
