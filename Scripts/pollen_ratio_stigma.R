3e7e0eb71c5b24a8eccbc2c1ef7db15f5fc7afd9
JoseBSL <barragansljose@gmail.com>
2018-08-31 12:10:01 +1000

    Species name added

--- Scripts/pollen_ratio_stigma.R
+++ Scripts/pollen_ratio_stigma.R
@@ -1,5 +1,6 @@
 
 library(ggplot2)
+library(reshape2)
 
 #All species (2 measurements per focal species. 40 days took me to measure all)
 focal_pollen_percentage <- as.matrix(read.table("Data/pollen_ratios_matrix.csv", header=TRUE, sep = ";",
@@ -18,76 +19,76 @@ non_focal_pollen_percentage$A <- "non_focal_pollen"
 
 pollen_ratios <- rbind(focal_pollen_percentage,non_focal_pollen_percentage)
 colnames(pollen_ratios) <- c("focal", "non_focal", "ratio", "variable")
-pollen_ratios$spp <- paste(pollen_ratios$focal,pollen_ratios$non_focal)
+#Changes spp code to common names
+
+data.frame(pollen_ratios, stringsAsFactors = F)
+pollen_ratios$focal <- as.character(pollen_ratios$focal)
+pollen_ratios$focal[pollen_ratios$focal=="CAAN"] <- "Capsicum"
+pollen_ratios$focal[pollen_ratios$focal=="SOLY"] <- "Tomato"
+pollen_ratios$focal[pollen_ratios$focal=="SOME"] <- "Eggplant"
+pollen_ratios$focal[pollen_ratios$focal=="PEIN"] <- "Petunia"
+pollen_ratios$focal[pollen_ratios$focal=="SIAL"] <- "White mustard"
+pollen_ratios$focal[pollen_ratios$focal=="ERSA"] <- "Rocket"
+pollen_ratios$focal[pollen_ratios$focal=="BROL"] <- "Wild cabbage"
+pollen_ratios$focal[pollen_ratios$focal=="BRRA"] <- "Pak choi"
+pollen_ratios$focal[pollen_ratios$focal=="IPAQ"] <- "Water morning glory"
+pollen_ratios$focal[pollen_ratios$focal=="IPPU"] <- "Morning glory"
+
+pollen_ratios$non_focal <- as.character(pollen_ratios$non_focal)
+pollen_ratios$non_focal[pollen_ratios$non_focal=="CAAN"] <- "Capsicum"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="SOLY"] <- "Tomato"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="SOME"] <- "Eggplant"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="PEIN"] <- "Petunia"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="SIAL"] <- "White mustard"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="ERSA"] <- "Rocket"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="BROL"] <- "Wild cabbage"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="BRRA"] <- "Pak choi"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="IPAQ"] <- "Water morning glory"
+pollen_ratios$non_focal[pollen_ratios$non_focal=="IPPU"] <- "Morning glory"
+
+pollen_ratios$spp <- paste(pollen_ratios$focal,pollen_ratios$non_focal, sep="-")
+
+write.csv(pollen_ratios, "Data/pollen_ratios.csv")
+
 
 ggplot(pollen_ratios, aes(x=spp, y=ratio, fill=variable)) +
-  geom_bar(stat='identity', position='dodge')
+  geom_bar(stat='identity', position='dodge')+theme(axis.text.x=element_text(angle=60,hjust=1))
 
 #Brassicaceae
 #Brassicaceae pollen ratio plot
+
+pollen_ratios <- read.csv("Data/pollen_ratios.csv")
 pollen_ratios_brassicaceae <- pollen_ratios[pollen_ratios$focal!="IPPU" & pollen_ratios$focal!="IPAQ"& pollen_ratios$focal!="SOME"&
                                pollen_ratios$focal!="SOLY" & pollen_ratios$focal!="PEIN"& pollen_ratios$focal!="CAAN",  ]
 
-write.csv(pollen_ratios_brassicaceae, "Rmd/Data/pollen_ratios_brassicaceae.csv")
+#write.csv(pollen_ratios_brassicaceae, "Rmd/Data/pollen_ratios_brassicaceae.csv")
 
 ggplot(pollen_ratios_brassicaceae, aes(x=spp, y=ratio, fill=variable)) +
-  geom_bar(stat='identity', position='dodge') +labs(title="Brassicaceae")
+  geom_bar(stat='identity', position='dodge') +labs(title="Brassicaceae") + theme(axis.text.x=element_text(angle=60,hjust=1))
 
 #Solanaceae pollen ratio plot
 
-#All species (2 measurements per focal species. 40 days took me to measure all)
-focal_pollen_percentage <- as.matrix(read.table("Data/pollen_ratios_matrix.csv", header=TRUE, sep = ";",
-                                                row.names = 1,
-                                                as.is=TRUE))
-focal_pollen_percentage <- melt(focal_pollen_percentage)
-focal_pollen_percentage <- focal_pollen_percentage[complete.cases(focal_pollen_percentage), ]
-focal_pollen_percentage$A <- "focal_pollen"
-non_focal_pollen_percentage <- as.matrix(read.table("Data/pollen_ratios_matrix_non_focal.csv", header=TRUE, sep = ";",
-                                                    row.names = 1,
-                                                    as.is=TRUE))
-
-non_focal_pollen_percentage <- melt(non_focal_pollen_percentage)
-non_focal_pollen_percentage <- non_focal_pollen_percentage[complete.cases(non_focal_pollen_percentage), ]
-non_focal_pollen_percentage$A <- "non_focal_pollen"
-
-pollen_ratios <- rbind(focal_pollen_percentage,non_focal_pollen_percentage)
-colnames(pollen_ratios) <- c("focal", "non_focal", "ratio", "variable")
-pollen_ratios$spp <- paste(pollen_ratios$focal,pollen_ratios$non_focal)
-
+pollen_ratios <- read.csv("Data/pollen_ratios.csv")
 pollen_ratios_solanaceae <- pollen_ratios[pollen_ratios$focal!="IPPU" & pollen_ratios$focal!="IPAQ"& pollen_ratios$focal!="BROL"&
                                  pollen_ratios$focal!="BRRA" & pollen_ratios$focal!="SIAL"& pollen_ratios$focal!="ERSA",  ]
 
-write.csv(pollen_ratios_solanaceae, "Rmd/Data/pollen_ratios_solanaceae.csv")
+#write.csv(pollen_ratios_solanaceae, "Rmd/Data/pollen_ratios_solanaceae.csv")
 
 ggplot(pollen_ratios_solanaceae, aes(x=spp, y=ratio, fill=variable)) +
-  geom_bar(stat='identity', position='dodge') +labs(title="Solanaceae")
+  geom_bar(stat='identity', position='dodge') +labs(title="Solanaceae") + theme(axis.text.x=element_text(angle=60,hjust=1))
 
 
 
 #Convolvulaceae
 
-focal_pollen_percentage <- as.matrix(read.table("Data/pollen_ratios_matrix.csv", header=TRUE, sep = ";",
-                                                row.names = 1,
-                                                as.is=TRUE))
-focal_pollen_percentage <- melt(focal_pollen_percentage)
-focal_pollen_percentage <- focal_pollen_percentage[complete.cases(focal_pollen_percentage), ]
-focal_pollen_percentage$A <- "focal_pollen"
-non_focal_pollen_percentage <- as.matrix(read.table("Data/pollen_ratios_matrix_non_focal.csv", header=TRUE, sep = ";",
-                                                    row.names = 1,
-                                                    as.is=TRUE))
+pollen_ratios <- read.csv("Data/pollen_ratios.csv")
 
-non_focal_pollen_percentage <- melt(non_focal_pollen_percentage)
-non_focal_pollen_percentage <- non_focal_pollen_percentage[complete.cases(non_focal_pollen_percentage), ]
-non_focal_pollen_percentage$A <- "non_focal_pollen"
-
-pollen_ratios <- rbind(focal_pollen_percentage,non_focal_pollen_percentage)
-colnames(pollen_ratios) <- c("focal", "non_focal", "ratio", "variable")
 pollen_ratios$spp <- paste(pollen_ratios$focal,pollen_ratios$non_focal)
 pollen_ratios_convolvulaceae <- pollen_ratios[pollen_ratios$focal!="CAAN" & pollen_ratios$focal!="SOME"& pollen_ratios$focal!="BROL"&
                                  pollen_ratios$focal!="BRRA" & pollen_ratios$focal!="SIAL"
                                & pollen_ratios$focal!="ERSA"& pollen_ratios$focal!="SOLY"& pollen_ratios$focal!="PEIN",  ]
 
-write.csv(pollen_ratios_convolvulaceae, "Rmd/Data/pollen_ratios_convolvulaceae.csv")
+#write.csv(pollen_ratios_convolvulaceae, "Rmd/Data/pollen_ratios_convolvulaceae.csv")
 
 ggplot(pollen_ratios_convolvulaceae, aes(x=spp, y=ratio, fill=variable)) +
-  geom_bar(stat='identity', position='dodge') +labs(title="Convolvulaceae")
+  geom_bar(stat='identity', position='dodge') +labs(title="Convolvulaceae") + theme(axis.text.x=element_text(angle=60,hjust=1))
