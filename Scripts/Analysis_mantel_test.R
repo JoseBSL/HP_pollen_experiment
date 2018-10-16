f29375390123fb1b201169960f7d08b56b8f6663
JoseBSL <barragansljose@gmail.com>
2018-10-16 17:03:23 +0200

    on the way to glory

--- Scripts/Analysis_mantel_test.R
+++ Scripts/Analysis_mantel_test.R
@@ -1,16 +1,40 @@
 #In this script I´m going to prepare the data to do some first Analysis
 
-#Load seed set data
+#Load seed set data for 10 species
 
-soly <- soly_seed_set <- read.csv("Data/species_seed_set/soly_seed_set.csv", sep=";")
-some <- some_seed_set <- read.csv("Data/species_seed_set/some_seed_set.csv", sep=";")
-pein <- pein_seed_set <- read.csv("Data/species_seed_set/pein_seed_set.csv", sep=";")
-caan <- caan_seed_set <- read.csv("Data/species_seed_set/caan_seed_set.csv", sep=";")
-ersa <- ersa_seed_set <- read.csv("Data/species_seed_set/ersa_seed_set.csv", sep=";")
-brra <- brra_seed_set <- read.csv("Data/species_seed_set/brra_seed_set.csv", sep=";")
-sial <- sial_seed_set <- read.csv("Data/species_seed_set/sial_seed_set.csv", sep=";")
-brol <- brol_seed_set <- read.csv("Data/species_seed_set/brol_seed_set.csv", sep=";")
-ippu <- ippu_seed_set <- read.csv("Data/species_seed_set/ippu_seed_set.csv", sep=";")
-ipaq <- ipaq_seed_set <- read.csv("Data/species_seed_set/ipaq_seed_set.csv", sep=";")
+soly  <- read.csv("Data/species_seed_set/soly_seed_set.csv", sep=";", stringsAsFactors = F)
+some  <- read.csv("Data/species_seed_set/some_seed_set.csv", sep=";", stringsAsFactors = F)
+pein  <- read.csv("Data/species_seed_set/pein_seed_set.csv", sep=";")
+caan  <- read.csv("Data/species_seed_set/caan_seed_set.csv", sep=";")
+ersa  <- read.csv("Data/species_seed_set/ersa_seed_set.csv", sep=";")
+brra  <- read.csv("Data/species_seed_set/brra_seed_set.csv", sep=";")
+sial  <- read.csv("Data/species_seed_set/sial_seed_set.csv", sep=";")
+brol  <- read.csv("Data/species_seed_set/brol_seed_set.csv", sep=";")
+ippu  <- read.csv("Data/species_seed_set/ippu_seed_set.csv", sep=";")
+ipaq  <- read.csv("Data/species_seed_set/ipaq_seed_set.csv", sep=";")
+
+#load libraries
+
+library(dplyr)
+str(soly)
 
 
+soly[-grep("100%", soly$Treatment), ]
+
+
+
+#To create the matrix of effect we are going to estimate the net effect as 
+#Cross-(average of 50% treatments)
+#I´m going to try to do it in a for loop
+
+species_list <- list(soly, some)
+
+i <- NULL
+y <- NULL
+for (i in species_list){
+
+ i <- filter(i, Treatment!="RARA 100%" & Treatment!="RARA 50%" )
+ i <-i[-grep("100", i$Treatment),] 
+  
+  y <- rbind(y, i)
+}
