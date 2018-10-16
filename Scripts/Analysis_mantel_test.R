c56a40af1ee5c8ac359f1b88125fe7b34ff965df
JoseBSL <barragansljose@gmail.com>
2018-10-16 15:55:44 +0200

    new script to prepare matrices for Mantel test

---
+++ Scripts/Analysis_mantel_test.R
@@ -0,0 +1,16 @@
+#In this script I´m going to prepare the data to do some first Analysis
+
+#Load seed set data
+
+soly <- soly_seed_set <- read.csv("Data/species_seed_set/soly_seed_set.csv", sep=";")
+some <- some_seed_set <- read.csv("Data/species_seed_set/some_seed_set.csv", sep=";")
+pein <- pein_seed_set <- read.csv("Data/species_seed_set/pein_seed_set.csv", sep=";")
+caan <- caan_seed_set <- read.csv("Data/species_seed_set/caan_seed_set.csv", sep=";")
+ersa <- ersa_seed_set <- read.csv("Data/species_seed_set/ersa_seed_set.csv", sep=";")
+brra <- brra_seed_set <- read.csv("Data/species_seed_set/brra_seed_set.csv", sep=";")
+sial <- sial_seed_set <- read.csv("Data/species_seed_set/sial_seed_set.csv", sep=";")
+brol <- brol_seed_set <- read.csv("Data/species_seed_set/brol_seed_set.csv", sep=";")
+ippu <- ippu_seed_set <- read.csv("Data/species_seed_set/ippu_seed_set.csv", sep=";")
+ipaq <- ipaq_seed_set <- read.csv("Data/species_seed_set/ipaq_seed_set.csv", sep=";")
+
+
