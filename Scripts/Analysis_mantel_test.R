88f24768d650f9da88640585ae44ac7ba8aebc0f
JoseBSL <barragansljose@gmail.com>
2018-10-18 12:31:41 +0200

    Now the two matrices are ready for analysis.

--- Scripts/Analysis_mantel_test.R
+++ Scripts/Analysis_mantel_test.R
@@ -3,7 +3,6 @@
 #In first place I modify the data to create a matrix
 
 #Load seed set data for 10 species
-
 soly  <- read.csv("Data/species_seed_set/soly_seed_set.csv", sep=";", stringsAsFactors = F)
 some  <- read.csv("Data/species_seed_set/some_seed_set.csv", sep=";", stringsAsFactors = F)
 pein  <- read.csv("Data/species_seed_set/pein_seed_set.csv", sep=";", stringsAsFactors = F)
@@ -15,8 +14,10 @@ brol  <- read.csv("Data/species_seed_set/brol_seed_set.csv", sep=";", stringsAsF
 ippu  <- read.csv("Data/species_seed_set/ippu_seed_set.csv", sep=";", stringsAsFactors = F)
 ipaq  <- read.csv("Data/species_seed_set/ipaq_seed_set.csv", sep=";", stringsAsFactors = F)
 
-#load libraries
+#load matrix of evolutionary distances
+evo_distance  <- read.csv("Data/species_matrix_phylogenetic_distance_rbcl.csv", sep=";", stringsAsFactors = F)
 
+#load libraries
 library(dplyr)
 library(reshape2)
 library(stringr)
@@ -33,7 +34,6 @@ library(stringr)
 some<- some[,-4]
 soly<- soly[,-4]
 caan<- caan[,-4]
-
 species_list <- list(soly, some, pein, caan, ersa, brra, sial, brol, ippu, ipaq)
 i <- NULL
 y <- NULL
@@ -54,8 +54,6 @@ colnames(i)<- c("Species", "Treatment", "Treatment_number", "Seed_set")
 mean(y[y$Species=="SOME", "Seed_set"])
 y_mean <- dcast(Species + Treatment ~ ., value.var = "Seed_set", fun.aggregate = mean, data = y, na.rm= TRUE)
 colnames(y_mean)[3] <- "Seed_set"
-
-
 Treatment <- str_split_fixed(as.character(y_mean$Treatment), " ", 2)
 Treatment <- Treatment[ , -2]
 y_mean <- cbind(y_mean, Treatment)
@@ -65,10 +63,7 @@ y_mean <- y_mean[ , -2]
 #y_mean$Treatment <- as.character(y_mean$Treatment)
 #y_mean$Species <- as.character(y_mean$Species)
 #y_mean$Seed_set <- as.numeric(y_mean$Seed_set)
-
-
 matrix <- tapply(y_mean$Seed_set, y_mean[c("Species", "Treatment")], mean)
-
 matrix<- matrix[-11,]
 
 #Fixing this value, don´t know why it doesn´t read it properly. I think it has an space or a weird format
@@ -94,9 +89,43 @@ y_cross_2 <- dcast(Species ~ ., value.var = "Seed_set", fun.aggregate = mean, da
 y_cross_3 <- dcast(Species ~ ., value.var = "Seed_set", fun.aggregate = mean, data = y_cross_3, na.rm= TRUE)
 y_cross <- rbind(y_cross_1, y_cross_2, y_cross_3)
 colnames(y_cross)[2] <- "Seed_set_cross"
-
 y_cross$Non_focal <- y_cross$Species
-
 matrix_cross <- tapply(y_cross$Seed_set_cross, y_cross[c("Species", "Non_focal")], mean)
 
+#I edit the matrix manually, values of the diagonal to full row and the diagonal NA
+matrix_cross[1,1:10] <- matrix_cross[1,1]#BROL
+matrix_cross[2,1:10] <- matrix_cross[2,2]#BRRA
+matrix_cross[3,1:10] <- matrix_cross[3,3]#CAAN
+matrix_cross[4,1:10] <- matrix_cross[4,4]#ERSA
+matrix_cross[5,1:10] <- matrix_cross[5,5]#IPAQ
+matrix_cross[6,1:10] <- matrix_cross[6,6]#IPPU
+matrix_cross[7,1:10] <- matrix_cross[7,7]#PEIN
+matrix_cross[8,1:10] <- matrix_cross[8,8]#SIAL
+matrix_cross[9,1:10] <- matrix_cross[9,9]#SOLY
+matrix_cross[10,1:10] <- matrix_cross[10,10]#SOME
+diag(matrix_cross) <- NA
 
+#The proxy of effect is the decrease in seed set 
+matrix_effect <- matrix/matrix_cross * 100
+matrix_effect[matrix_effect[,]>100] <-100
+#To make it more intuitive, 100% 0 percent of seed set
+matrix_effect <- 100-matrix_effect
+
+#Now edit the evolutionary distances
+
+evo_distance <- evo_distance[ , -2]
+evo_distance <- as.matrix(evo_distance)
+
+rownames(evo_distance) <- c("SIAL", "ERSA", "BROL", "BRRA", "IPPU", "IPAQ", "PEIN", "CAAN", "SOLY", "SOME")
+evo_distance <- evo_distance[, -1]
+colnames(evo_distance) <- c("SIAL", "ERSA", "BROL", "BRRA", "IPPU", "IPAQ", "PEIN", "CAAN", "SOLY", "SOME")
+diag(evo_distance) <- 0
+
+makeSymm <- function(evo_distance) {
+  evo_distance[upper.tri(evo_distance)] <- t(evo_distance)[upper.tri(evo_distance)]
+  return(evo_distance)
+}
+evo_distance <- makeSymm(evo_distance)
+diag(evo_distance) <- NA
+
+evo_distance[order(rownames(evo_distance)), order(colnames(evo_distance))] 
