b3f814b53ec3680e87f32f82672e30a4bf4aceef
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2018-12-10 12:53:49 +1100

    file org

---
+++ Scripts/dir and file org.R
@@ -0,0 +1,7 @@
+#dir.create("Scripts/Species")
+
+#file.rename("Scripts/brassica_oleracea_100.R", "Scripts/Species/brassica_oleracea_100.R")
+
+#file.rename("Scripts/brassica_rapa_100.R", "Scripts/Species/brassica_rapa_100.R")
+
+#file.rename("Scripts/eruca_versicaria_100.R", "Scripts/Species/eruca_versicaria_100.R")
