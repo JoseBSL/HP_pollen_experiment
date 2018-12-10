5e2b3513bb535d2f97e55ade51df26f01785540c
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2018-12-10 15:30:14 +1100

    caan 100%

---
+++ Scripts/Species/capsicum_annuum_100.R
@@ -0,0 +1,113 @@
+
+#
+##
+###
+#C. ANNUUM 
+###
+##
+#
+caan_seed_set <- read.csv("Data/species_seed_set/CAAN_seed_set.csv", sep=";")
+
+#adding the focal species, first 50%
+Species <- rep("CAAN", 10)
+Treatment <- rep("CAAN 50%", 10)
+Treatment.number <- seq(1:10)
+Seed.production <- rep(0, 10)
+caan_50 <- data.frame(Species,Treatment, Treatment.number, Seed.production)
+#adding the focal species, 100%
+Species <- rep("CAAN", 10)
+Treatment <- rep("CAAN 100%", 10)
+Treatment.number <- seq(1:10)
+Seed.production <- rep(0, 10)
+caan_100<-  data.frame(Species,Treatment, Treatment.number, Seed.production)
+#Order from lower to higher values the average, to plot it nicely
+caan_seed_set <- caan_seed_set[,-4]
+colnames(caan_seed_set)[1] <- "Species"
+colnames(caan_seed_set)[2] <- "Treatment"
+colnames(caan_seed_set)[3] <- "Treatment.number"
+colnames(caan_seed_set)[4] <- "Seed.production"
+caan_seed_set <- rbind(caan_seed_set, caan_50, caan_100)
+caan_seed_set$Treatment<- as.character(caan_seed_set$Treatment)
+caan_seed_set <- caan_seed_set[order(caan_seed_set$Treatment),]  
+
+#Organizing with just 50% and cross, self, flower control and control on a side
+
+library(stringr)
+caan_seed_set_div <- str_split_fixed(as.character(caan_seed_set$Treatment), " ", 2)
+caan_seed_set_div <- data.frame(caan_seed_set_div, stringsAsFactors = F)
+colnames(caan_seed_set_div)=c("non_focal","percentage")
+caan_seed_set_bind <- cbind(caan_seed_set,caan_seed_set_div)
+caan_seed_set_bind <- filter(caan_seed_set_bind, percentage!="50%")
+
+#Because I want to give it specifically order I do it separately
+#pein_seed_set_common <- filter(caan_seed_set_bind, non_focal %in% c("CROSS", "SELF","CONTROL","FLOWER"))
+
+caan_seed_set_cross <- filter(caan_seed_set_bind, non_focal %in% c("CROSS"))
+caan_seed_set_self <- filter(caan_seed_set_bind, non_focal %in% c("SELF"))
+caan_seed_set_control <- filter(caan_seed_set_bind, non_focal %in% c("CONTROL"))
+caan_seed_set_flower <- filter(caan_seed_set_bind, non_focal %in% c("FLOWER"))
+
+#Changing 0 for NA'S FOCAL species
+
+
+
+
+# I HAVENT BEEN ABLE TO WORK OUT ALL AT THE SAME TIME SO i DO IOT SEPARATELY
+caan_seed_set_bind=caan_seed_set_bind[caan_seed_set_bind$non_focal!=c("CROSS"),]
+caan_seed_set_bind=caan_seed_set_bind[caan_seed_set_bind$non_focal!=c("SELF"),]
+caan_seed_set_bind=caan_seed_set_bind[caan_seed_set_bind$non_focal!=c("CONTROL"),]
+caan_seed_set_bind=caan_seed_set_bind[caan_seed_set_bind$non_focal!=c("FLOWER"),]
+
+caan_seed_set_final<-rbind(caan_seed_set_bind,caan_seed_set_cross, caan_seed_set_self, caan_seed_set_control,
+                           caan_seed_set_flower)
+#deleting extra columns used for data formating
+caan_seed_set_final=caan_seed_set_final[,-c(5,6)]
+#adding NA'S to the focal species
+caan_seed_set_final[caan_seed_set_final$Treatment==c("CAAN 50%"),4] <- NA
+
+#changing non_focal species name
+caan_seed_set_final$Treatment=as.character(caan_seed_set_final$Treatment)
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="BROL 100%"] <- "Brassicaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="BRRA 100%"] <- "Brassicaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="SIAL 100%"] <- "Brassicaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="ERSA 100%"] <- "Brassicaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="SOME 100%"] <- "Solanaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="SOLY 100%"] <- "Solanaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="CAAN 100%"] <- "Solanaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="PEIN 100%"] <- "Solanaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="IPPU 100%"] <- "Convolvulaceae"
+caan_seed_set_final$Family[caan_seed_set_final$Treatment=="IPAQ 100%"] <- "Convolvulaceae"
+
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="BROL 50%"] <- "Brassica oleracea"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="BRRA 50%"] <- "Brassica rapa"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="CAAN 50%"] <- "Capsicum annuum"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="ERSA 50%"] <- "Eruca vesicaria" #Eruca sativa seems to be a synonym
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="IPAQ 50%"] <- "Ipomoea aquatica"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="IPPU 50%"] <- "Ipomoea purpurea"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="PEIN 50%"] <- "Petunia integrifolia"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="SIAL 50%"] <- "Sinapis alba"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="SOLY 50%"] <- "Solanum lycopersicum"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="SOME 50%"] <- "Solanum melongena"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="CROSS"] <- "Cross"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="SELF"] <- "Self"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="CONTROL"] <- "Control"
+#caan_seed_set_final$Treatment[caan_seed_set_final$Treatment=="FLOWER CONTROL"] <- "Flower control"
+
+caan_seed_set_brassicaceae <- filter(caan_seed_set_final, Family %in% c("Brassicaceae"))
+caan_seed_set_convolvulaceae <- filter(caan_seed_set_final, Family %in% c("Convolvulaceae"))
+caan_seed_set_solanaceae <- filter(caan_seed_set_final, Family %in% c("Solanaceae"))
+caan_seed_set_final$Family[is.na(caan_seed_set_final$Family)] <- "Solanum lycopersicum"
+caan_seed_set_cross=caan_seed_set_cross[,-c(5,6)]
+caan_seed_set_cross$Family <- "other"
+caan_seed_set_self=caan_seed_set_self[,-c(5,6)]
+caan_seed_set_self$Family <- "other"
+caan_seed_set_control=caan_seed_set_control[,-c(5,6)]
+caan_seed_set_control$Family <- "other"
+caan_seed_set_flower=caan_seed_set_flower[,-c(5,6)]
+caan_seed_set_flower$Family <- "other"
+
+caan_seed_set_final=rbind(caan_seed_set_brassicaceae, caan_seed_set_convolvulaceae, 
+                          caan_seed_set_solanaceae, caan_seed_set_cross, caan_seed_set_self, caan_seed_set_control,
+                          caan_seed_set_flower)
+
+write.csv(caan_seed_set_final, "Rmd/Data/caan_seed_set_100.csv")
