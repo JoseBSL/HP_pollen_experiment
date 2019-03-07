ad9718e1db4e26f3b85d8071cd6d867bb49d8cc3
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-03-07 11:28:01 +1100

    chequing glm with traits

--- Scripts/Analysis/Analysis_glm.R
+++ Scripts/Analysis/Analysis_glm.R
@@ -4,6 +4,20 @@
 hp <- read.csv("Data/y.csv")
 hp_mean <- read.csv("Data/y_mean_scale.csv")
 
+#Now I'm using the HP effect calculated in Analysis_mantel_test
+#But maybe I could use later effect sizes...
+
 #Now I'm going to match all the traits with these data.frames
+traits_all <- read.csv("Data/traits_all.csv", sep=",")
+#I create a column with equal code of species in order to merge
+traits_all$species <- c("BROL", "BRRA", "CAAN", "ERSA", "IPAQ", "IPPU", 
+                        "PEIN", "SIAL", "SOLY", "SOME") 
+#colname to merge
+colnames(traits_all)[2] <- "Species"
+
+#I use the mean effect for each treatment, later maybe I come back and use the other
+data <- merge(hp_mean, traits_all, by="Species")
+
+#The column is selfing rate I'm going to add SI values
 
 
