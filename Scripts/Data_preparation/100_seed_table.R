a6105d3958aa797c4db73e0e7e7c435ad2be4f64
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-03-17 14:13:34 +1100

    edits on table headers

--- Scripts/Data_preparation/100_seed_table.R
+++ Scripts/Data_preparation/100_seed_table.R
@@ -4,7 +4,7 @@
 
 #First for Brassicaceae specie
 Family <- c(rep("Brassicaceae",9)) 
-Species <- c("B. oleracea", rep("B. rapa", 5), "S. alba", "E. sativa","E. sativa")
+Recipient <- c("B. oleracea", rep("B. rapa", 5), "S. alba", "E. sativa","E. sativa")
 Donor <- c("C. annuum", rep("B. oleracea",2), "S. lycopersicum",rep("B. oleracea",3),
            "C. annuum","C. annuum") 
 Seeds <-  c(5, 2, 13, 1, 7, 5, 7, 6, 1)
@@ -13,7 +13,7 @@ dat_bra <- data.frame(Family, Species, Donor, Seeds)
 
 #Solanaceae species
 Family <- c(rep("Solanaceae",4)) 
-Species <- c("S. lycopersicum", "S. melongena", "C. annuum", "C. annuum")
+Recipient <- c("S. lycopersicum", "S. melongena", "C. annuum", "C. annuum")
 Donor <- c("S. alba", "P. integrifolia", "S. alba", "E. sativa")
 Seeds <-  c(3, 36, 127,3)
 
