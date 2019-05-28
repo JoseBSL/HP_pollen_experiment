acdb58e65397c525d109c14c5429ec93a04fc451
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-05-28 18:33:41 +1000

    adding to analysis mantel family more analysis

--- Scripts/Analysis/Analysis_mantel_family.R
+++ Scripts/Analysis/Analysis_mantel_family.R
@@ -21,64 +21,122 @@ effect_size=melt(matrix_effect_size)
 #Also with effect sizes
 matrix_soly=matrix_scale_effect[c(3,7,9,10),c(3,7,9,10)]
 matrix_soly_1=matrix_effect_size[c(3,7,9,10),c(3,7,9,10)]
-
 traits_sola=traits_all[c(3,7,9,10),-c(1,2)]
-traits_sola_dist <- dist(traits_sola, diag = T)
-a <- mantel(matrix_soly,traits_sola_dist)
-summary(a)
-mantel(matrix_soly,dist(traits_sola[,6]))
-mantel(matrix_soly_1,dist(traits_sola[,6]))
-
+#scale traits previously analysis
+traits_sola=traits_sola[,-c(1,7,11)]
+traits_sola=scale(traits_sola)
+traits_sola_dist <- dist(traits_sola)
+#Mantel vegan package
+mantel(dist(matrix_soly),traits_sola_dist)
+#Mantel ecodist package
+mantel(matrix_soly, traits_sola_dist)
+trial=traits_all[,5:10]
+traits_sola=as.data.frame(traits_sola)
+#maximum rank analysis bioenv
+bioenv(matrix_soly,traits_sola)
+#Best rank correlation: Mean pollen per anther, style length and si index
+#I select these 3 and run mantel with them
+best_rank=traits_sola[,c(3,10,14)]
+mantel(matrix_soly,dist(best_rank))
+#r=-0.79, p=1
+#looking for significance among the maximum rank variables
+str(traits_sola)
 cor.test(sola$Cohen_d,sola$pollen_size)
-
+traits_sola=scale(traits_sola)
 #Now I perform Mantel test between Hp effect matrix and trait by trait
-#stigma_type
+#Selfing rate
 mantel(matrix_soly,dist(traits_sola[,1]))
+#mantel(dist(matrix_soly),dist(traits_sola[,1]))
+#matrix and istance matrix of Hp, checking if the results are different
 mantel(matrix_soly_1,dist(traits_sola[,1]))
+protest(dist(matrix_soly),dist(traits_sola[,1]))
 
-#Selfing_rate
+
+#This last one is with effect sizes
+#Pollen size
 mantel(matrix_soly,dist(traits_sola[,2]))
+#mantel(dist(matrix_soly),dist(traits_sola[,2]))
+#Effect size Mantel
 mantel(matrix_soly_1,dist(traits_sola[,2]))
-
-#pollen_size
-mantel(matrix_soly,dist(traits_sola[,3]))
-mantel(matrix_soly_1,dist(traits_sola[,3]))
+protest(dist(matrix_soly),dist(traits_sola[,2]))
 
 #mean_pollen_anther
-mantel(matrix_soly,dist(traits_sola[,4]))
-mantel(matrix_soly_1,dist(traits_sola[,4]))
+mantel(matrix_soly,dist(traits_sola[,3]))
+#mantel(dist(matrix_soly),dist(traits_sola[,3]))
+mantel(matrix_soly_1,dist(traits_sola[,3]))
+protest(dist(matrix_soly),dist(traits_sola[,3]))
 
 #mean_ovules
-mantel(matrix_soly,dist(traits_sola[,5]))
-mantel(matrix_soly_1,dist(traits_sola[,5]))
+mantel(matrix_soly,dist(traits_sola[,4]))
+#mantel(dist(matrix_soly),dist(traits_sola[,4]))
+mantel(matrix_soly_1,dist(traits_sola[,4]))
+protest(dist(matrix_soly),dist(traits_sola[,4]))
 
 #pollen_ovule_ratio
-mantel(matrix_soly,dist(traits_sola[,6]))
-mantel(matrix_soly_1,dist(traits_sola[,6]))
+mantel(matrix_soly,dist(traits_sola[,5]))
+#mantel(dist(matrix_soly),dist(traits_sola[,5]))
+mantel(matrix_soly_1,dist(traits_sola[,5]))
+protest(dist(matrix_soly),dist(traits_sola[,5]))
 
-#anthers
-mantel(matrix_soly,dist(traits_sola[,7]))
 #stigma_area
-mantel(matrix_soly,dist(traits_sola[,8]))
+mantel(matrix_soly,dist(traits_sola[,6]))
+#mantel(dist(matrix_soly),dist(traits_sola[,6]))
+mantel(matrix_soly_1,dist(traits_sola[,6]))
+protest(dist(matrix_soly),dist(traits_sola[,6]))
+
+
 #stigma_length
-mantel(matrix_soly,dist(traits_sola[,9]))
+mantel(matrix_soly,dist(traits_sola[,7]))
+#mantel(dist(matrix_soly),dist(traits_sola[,7]))
+mantel(matrix_soly_1,dist(traits_sola[,7]))
+protest(dist(matrix_soly),dist(traits_sola[,7]))
+
+
+
 #stigma_width
-mantel(matrix_soly,dist(traits_sola[,10]))
-#stigma_surface
-mantel(matrix_soly,dist(traits_sola[,11]))
+mantel(matrix_soly,dist(traits_sola[,8]))
+#mantel(dist(matrix_soly),dist(traits_sola[,8]))
+mantel(matrix_soly_1,dist(traits_sola[,8]))
+protest(dist(matrix_soly),dist(traits_sola[,8]))
+
+
 #style_length
-mantel(matrix_soly,dist(traits_sola[,12]))
+mantel(matrix_soly,dist(traits_sola[,9]))
+mantel(dist(matrix_soly),dist(traits_sola[,9]))
+mantel(matrix_soly_1,dist(traits_sola[,9]))
+protest(dist(matrix_soly),dist(traits_sola[,9]))
+
+
 #style_width
-mantel(matrix_soly,dist(traits_sola[,13]))
+mantel(matrix_soly,dist(traits_sola[,10]))
+#mantel(dist(matrix_soly),dist(traits_sola[,10]))
+mantel(matrix_soly_1,dist(traits_sola[,10]))
+protest(dist(matrix_soly),dist(traits_sola[,10]))
+
+
 #ovary_width
-mantel(matrix_soly,dist(traits_sola[,14]))
+mantel(matrix_soly,dist(traits_sola[,11]))
+#mantel(dist(matrix_soly),dist(traits_sola[,11]))
+mantel(matrix_soly_1,dist(traits_sola[,11]))
+protest(dist(matrix_soly),dist(traits_sola[,11]))
+
+
 #ovary_length
-mantel(matrix_soly,dist(traits_sola[,15]))
+mantel(matrix_soly,dist(traits_sola[,12]))
+#mantel(dist(matrix_soly),dist(traits_sola[,12]))
+mantel(matrix_soly_1,dist(traits_sola[,12]))
+protest(dist(matrix_soly),dist(traits_sola[,12]))
+
+
 #si_index
-mantel(matrix_soly,dist(traits_sola[,16]))
+mantel(matrix_soly,dist(traits_sola[,13]))
+mantel(dist(matrix_soly),dist(traits_sola[,13]))
+mantel(matrix_soly_1,dist(traits_sola[,13]))
+protest(dist(matrix_soly),dist(traits_sola[,13]))
 
 
-
+#Simple Pearson test between traits and effect
+#Style length seems to be the more interesting trait
 
 traits_sola$species <- c("CAAN", "PEIN","SOLY","SOME")
 effect_size_sola=effect_size_all[c(1:4),c(1,2)]
@@ -111,39 +169,67 @@ ggscatter(sola, x = "values", y = "style_length",
 matrix_brra=matrix_scale_effect[c(1,2,4,8),c(1,2,4,8)]
 traits_brra=traits_all[c(1,2,4,8),-c(1,2)]
 brra=effect_size_all[c(7:10),c(1,2)]
-mantel(matrix_brra,dist(traits_brra[,1]))
-cor.test(brra$Cohen_d,traits_brra$mean_ovules)
-cor.test(brra$Cohen_d,traits_brra$mean_ovules)
+#scale traits and removed unused variables (stigma, type, number of anthers)
+traits_brra=scale(traits_brra[,-c(1,7,11)])
+mantel(matrix_brra,dist(traits_brra))
 
-#pollen size
+#
+cor.test(brra$Cohen_d,traits_brra$mean_ovules)
+cor.test(brra$Cohen_d,traits_brra$mean_ovules)
+bioenv(matrix_brra,traits_brra)
+
+#Selfing_rate
 mantel(matrix_brra,dist(traits_brra[,1]))
-#mean_pollen_anther
+protest(dist(matrix_brra),dist(traits_brra[,1]))
+
+#pollen_size
 mantel(matrix_brra,dist(traits_brra[,2]))
-#mean_ovules
+protest(dist(matrix_brra),dist(traits_brra[,2]))
+
+#mean_pollen_anther
 mantel(matrix_brra,dist(traits_brra[,3]))
-#Mantel statistic r: 0.3997; 0.041667 
-#pollen_ovule_ratio
+protest(dist(matrix_brra),dist(traits_brra[,3]))
+
+#mean_ovules
 mantel(matrix_brra,dist(traits_brra[,4]))
-#anthers
+protest(dist(matrix_brra),dist(traits_brra[,4]))
+
+#pollen_ovule_ratio
 mantel(matrix_brra,dist(traits_brra[,5]))
+protest(dist(matrix_brra),dist(traits_brra[,5]))
+
 #stigma_area
 mantel(matrix_brra,dist(traits_brra[,6]))
+protest(dist(matrix_brra),dist(traits_brra[,6]))
+
 #stigma_length
 mantel(matrix_brra,dist(traits_brra[,7]))
+protest(dist(matrix_brra),dist(traits_brra[,7]))
+
 #stigma_width
 mantel(matrix_brra,dist(traits_brra[,8]))
-#stigma_surface
-mantel(matrix_brra,dist(traits_brra[,9]))
+protest(dist(matrix_brra),dist(traits_brra[,8]))
+
 #style_length
-mantel(matrix_brra,dist(traits_brra[,10]))
+mantel(matrix_brra,dist(traits_brra[,9]))
+protest(dist(matrix_brra),dist(traits_brra[,9]))
+
 #style_width
-mantel(matrix_brra,dist(traits_brra[,11]))
+mantel(matrix_brra,dist(traits_brra[,10]))
+protest(dist(matrix_brra),dist(traits_brra[,10]))
+
 #ovary_width
-mantel(matrix_brra,dist(traits_brra[,12]))
+mantel(matrix_brra,dist(traits_brra[,11]))
+protest(dist(matrix_brra),dist(traits_brra[,11]))
+
 #ovary_length
-mantel(matrix_brra,dist(traits_brra[,13]))
+mantel(matrix_brra,dist(traits_brra[,12]))
+protest(dist(matrix_brra),dist(traits_brra[,12]))
+
 #si_index
-mantel(matrix_brra,dist(traits_brra[,14]))
+mantel(matrix_brra,dist(traits_brra[,13]))
+protest(dist(matrix_brra),dist(traits_brra[,13]))
+
 
 
 ####
@@ -161,8 +247,17 @@ save.image("Manuscript_draft/Data/family_cor.RData")
 
 
 
-#BRRA and SOLY
+#Example Mantel test from ecodist package
+library("ecodist")
+set.seed(NULL)
 
-brra
-sola
-
+set.seed(9876)
+sampleloc <- 1:20
+species <- matrix(rnorm(100), nrow = 20, ncol = 5)
+sampleloc.edist <- distance(sampleloc, "euclidean")
+species.bcdist <- distance(species, "bray-curtis")
+mantel(species.bcdist ~ sampleloc.edist)
+traits_sola=distance(traits_sola, "bray-curtis")
+matrix_soly <- distance(matrix_soly, "euclidean")
+#Ok now I'm able to running it
+mantel(matrix_soly~ traits_sola, nperm = 1000, nboot = 500, pboot = 0.9, cboot = 0.95)
