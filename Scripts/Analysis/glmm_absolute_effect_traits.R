ef9a4b4a3588ac1a2aaf91e2d2f3e6e315e3bccb
Jose Barragán Sánchez-Lanuza <lanuza@joses-imac.home>
2019-06-14 16:13:55 +1000

    changes in the model

--- Scripts/Analysis/glmm_absolute_effect_traits.R
+++ Scripts/Analysis/glmm_absolute_effect_traits.R
@@ -1,16 +1,23 @@
 #In this script I'm going to calculate the absolute effect of traits
 #First I have to prepare the data in the desire format for the analysis
 
-#load library
+#load libraries
 library(reshape2)
 library(lme4)
+library(ggplot2)
+library(fBasics)
+library(lmerTest)
+#
+##
+###DATA PREPARATION FOR ANALYSES
+##
+#
 
 #load effect sizes
 matrix_effect_size <- readRDS("Data/matrix_effect_size.RData")
 e_size <- melt(matrix_effect_size)
 e_size <- as.data.frame(e_size, stringsAsFactors=FALSE)
 #Structure of our data
-library(ggplot2)
 ggplot(e_size, aes(x = value)) + geom_density() 
 #seems that a poisson could fit nicely
 #load traits
@@ -49,43 +56,66 @@ effect_recipient$Donor[effect_recipient$Donor=="SOME "] <-"SOME"
 mydata<- merge(effect_recipient, traits_donor, by="Donor")
 mydata$D <- rep(1:10, each=10)
 colnames(data)[3] <- "effect_size"
-#ANALYSE
-fit <- glm(value~Donor_pollen_size,data=mydata,family=gaussian)
-summary(fit)
-confint(fit)
-exp(coef(fit))
-predict(fit, type="response")
-residuals(fit, type="deviance")
 
-str(mydata)
-mydata$Donor=as.factor(mydata$Donor)
-plot(Oats)
-#following an example
+#Now the data is ready for analysis
+
+#
+##
+#ANALYSES
+##
+#
+
+#Perform a Gaussian glmm-> lmer
+#Random effects are Donor and recipientdue due to they where not alway the same individuals
+model1<-lmer(value~Recipient_stigma_area*Donor_pollen_size+ (1|Donor)+ (1|Recipient),data=mydata,REML=FALSE)
+summary(model1)
+
+model2<-lmer(value~Recipient_style_length*Donor_pollen_size+Recipient_pollen_ovule_ratio+ (1|Donor)+ (1|Recipient),data=mydata,REML=FALSE)
+summary(model2)
+
+model3<-lmer(value~Donor_pollen_size*Recipient_pollen_size+Recipient_style_length+ (1|Donor)+ (1|Recipient),data=mydata,REML=FALSE)
+summary(model3)
+
+model3<-lmer(value~Donor_si_index+Recipient_ovary_width+ (1|Donor)+ (1|Recipient),data=mydata,REML=FALSE)
+summary(model3)
+
+model3<-lmer(value~ Donor_pollen_size*Recipient_style_length+  (1|Donor)+ (1|Recipient),data=mydata,REML=FALSE)
+summary(model3)
+
+model3<-lmer(value~Donor+ (1|Recipient),data=mydata,REML=FALSE)
+summary(model3)
+
+#Interpreting outputs:
+#Random effects, Recipient have a big standard deviation which means that great part of variability
+#of our model can be explained by them. 
+
 
 model1<-lmer(value~1 + (1|D),data=mydata,REML=FALSE)
 model3<-lmer(value~Recipient_si_index + (1|Donor)+ (1|Recipient),data=mydata,REML=FALSE)
 
-model2<-lmer(value~Recipient_stigma_area*Donor_pollen_size+Recipient_style_length*Donor_style_length + (1|Donor)+ (1|Recipient),data=mydata,REML=FALSE)
 #How to obtain a p-value by comparing with a "null" model...
 
+#Analysing residuals
 plot(model2)
 qqnorm(residuals(model2))
+jarqueberaTest(model2$df.resid)
 
+#Same analysis without Convolvulaceae
+
+mydata
+library(dplyr)
+filter_data <- filter(mydata, Recipient != "IPPU" & Recipient != "IPAQ")
+filter_data <- filter(filter_data, Donor != "IPPU" & Donor != "IPAQ")
+
+
+model1<-lmer(value~Recipient_stigma_area*Donor_pollen_size+ (1|Donor)+ (1|Recipient),data=filter_data,REML=FALSE)
 summary(model1)
+
+model1<-lmer(value~Recipient_stigma_area*Donor_pollen_size+ (1|Donor)+ (1|Recipient),data=filter_data,REML=FALSE)
+summary(model1)
+
+model2<-lmer(value~Recipient_style_length*Donor_pollen_size+Recipient_pollen_ovule_ratio+ (1|Donor)+ (1|Recipient),data=filter_data,REML=FALSE)
 summary(model2)
+
+model3<-lmer(value~Donor_pollen_size*Recipient_pollen_size+Recipient_style_length+ (1|Donor)+ (1|Recipient),data=filter_data,REML=FALSE)
 summary(model3)
-anova(model1,model2)
-anova(model1,model3)
-
-coef(model1)
-
-ggplot(data.frame(eta=predict(model2,type="link"),pearson=residuals(model2,type="pearson")),
-       aes(x=eta,y=pearson)) +
-  geom_point() +
-  theme_bw()
-
-
-ggplot(data.frame(x1=model2$x1,pearson=residuals(model2,type="pearson")),
-       aes(x=x1,y=pearson)) +
-  geom_point() +
-  theme_bw()
