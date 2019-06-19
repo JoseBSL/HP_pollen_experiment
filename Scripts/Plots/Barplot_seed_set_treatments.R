3604a0e90d09ea1c0950a1e4adb17386aca4d958
JoseBSL <barragansljose@gmail.com>
2019-06-19 17:53:44 +1000

    edits

--- Scripts/Plots/Barplot_seed_set_treatments.R
+++ Scripts/Plots/Barplot_seed_set_treatments.R
@@ -12,11 +12,17 @@ library(reshape2)
 load("Manuscript_draft/Data/table_treatments.RData")
 
 Species <- c("Brassica oleracea", "Brassica rapa", "Eruca versicaria", "Sinapis alba", "Ipomoea aquatica", "Ipomoea purpurea", "Capsicum annuum", "Petunia integrifolia", "Solanum lycopersicum", "Solanum melongena")
-Hand_cross_pollination <- cross
-Hand_self_pollination <- self
-Natural_selfing <- nat_self
-Apomixis <- apo
-table<- data.frame(Species, Hand_cross_pollination, Hand_self_pollination, Natural_selfing,Apomixis)
+Hand_cross_pollination_mean <- cross_mean
+Hand_self_pollination_mean <- self_mean
+Natural_selfing_mean <- nat_self_mean
+Apomixis_mean <- apo_mean
+table<- data.frame(Species, Hand_cross_pollination_mean, Hand_self_pollination_mean, Natural_selfing_mean,Apomixis_mean)
+Hand_cross_pollination_sd <- cross_sd
+Hand_self_pollination_sd <- self_sd
+Natural_selfing_sd <- nat_self_sd
+Apomixis_sd <- apo_sd
+table_1<- data.frame(Species, cross_sd, self_sd, nat_self_sd, apo_sd)
+
 
 #I try a second way with ggplot
 table_melt=melt(table) 
@@ -28,6 +34,14 @@ table_melt$variable[table_melt$variable=="Hand_self_pollination"] <- "Hand self 
 table_melt$variable[table_melt$variable=="Natural_selfing"] <- "Natural selfing"
 table_melt$variable[table_melt$variable=="Apomixis"] <- "Apomixis"
 
+table_melt_1=melt(table_1)
+table_melt_1$value[table_melt_1$value==0] <- 0.5
+
+table_melt$variable=as.character(table_melt$variable)
+table_melt$variable[table_melt$variable=="Hand_cross_pollination"] <- "Hand cross pollination"
+table_melt$variable[table_melt$variable=="Hand_self_pollination"] <- "Hand self pollination"
+table_melt$variable[table_melt$variable=="Natural_selfing"] <- "Natural selfing"
+table_melt$variable[table_melt$variable=="Apomixis"] <- "Apomixis"
 #install.packages("extrafont")
 library(extrafont)
 library(forcats)
@@ -55,12 +69,18 @@ cbp1 <- c("#999999","#0072B2", "#E69F00", "#009E73",
           "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 str(bio)
+bio<-cbind(bio,table_melt_1)
+bio=bio[,-c(4,5)]
+colnames(bio)[3]<- "seed_set_mean"
+colnames(bio)[4]<- "seed_set_sd"
+
 bio$Species=as.character(bio$Species)
 bio$Species[bio$Species=="Eruca versicaria"]<- "Eruca sativa"
 bio$Species <- factor(bio$Species, levels = rev(unique(bio$Species)), ordered=TRUE)
-ggplot(bio,aes(x=Species,y=value,fill=variable))+
+ggplot(bio,aes(x=Species,y=seed_set_mean,fill=variable))+
   geom_bar(stat="identity",position="dodge")+scale_fill_grey(start = 0, end = 0.85,name="Treatments") + 
   theme_grey()+xlab("Species")+ylab("Seed-setting rate (%)")+labs(title="Spp Reproductive Biology")+
   theme(axis.text.y = element_text(angle = 0, hjust = 1, face= "italic",family = "Arial Narrow"))+
   scale_y_continuous(expand = c(0, 0),minor_breaks = seq(0 , 100, 5), breaks = seq(0, 100, 10))+
-  scale_fill_manual(values = cbp1,name = "Treatments")+coord_flip()
+  scale_fill_manual(values = cbp1,name = "Treatments")+ 
+  geom_errorbar(aes(ymin=seed_set_mean-seed_set_sd, ymax=seed_set_mean+seed_set_sd), width=.2, position=position_dodge(.9)) 
