f0cd3d022b1f0e3e8c2952946fe7ca21be0a8f59
Jose Barragán Sánchez-Lanuza <lanuza@joses-imac.home>
2019-06-20 19:47:59 +1000

    edits

--- Scripts/Analysis/Analysis_Mantel.R
+++ Scripts/Analysis/Analysis_Mantel.R
@@ -56,13 +56,12 @@ mantel(matrix_effect_size,dist(traits_all_scaled[,13]))
 #Effect size~phylogenetic distance
 mantel(matrix_effect_size,evo_distance_its)
 mantel(matrix_effect_size,evo_distance_rbcl)
-
 #Now by family 
 matrix_soly=matrix_effect_size[c(3,7,9,10),c(3,7,9,10)]
 traits_soly=traits_all_scaled[c(3,7,9,10),]
 
 #SOLANACEAE
-mantel(matrix_soly,traits_soly)                       
+mantel(matrix_soly,traits_soly)
 #Selfing rate
 mantel(matrix_soly,dist(traits_soly[,1]))
 #Pollen size
@@ -124,3 +123,116 @@ mantel(matrix_brra,dist(traits_brra[,11]))
 mantel(matrix_brra,dist(traits_brra[,12]))
 #SI index
 mantel(matrix_brra,dist(traits_brra[,13]))
+
+
+#
+#
+#PROTEST
+#
+#
+#
+##ANALYSIS
+#
+
+#Effect size~traits
+
+#First all the traits
+protest(matrix_effect_size,dist(traits_all_scaled))
+#Now trait by trait
+#Selfing rate
+protest(matrix_effect_size,dist(traits_all_scaled[,1]))
+#Pollen size
+protest(matrix_effect_size,dist(traits_all_scaled[,2]))
+#Pollen per anther
+protest(matrix_effect_size,dist(traits_all_scaled[,3]))
+#Ovules
+protest(matrix_effect_size,dist(traits_all_scaled[,4]))
+#Pollen ovule ratio
+protest(matrix_effect_size,dist(traits_all_scaled[,5]))
+#Stigmatic area
+protest(matrix_effect_size,dist(traits_all_scaled[,6]))
+#Stigma length
+protest(matrix_effect_size,dist(traits_all_scaled[,7]))
+#Stigma width
+protest(matrix_effect_size,dist(traits_all_scaled[,8]))
+#Style length
+protest(matrix_effect_size,dist(traits_all_scaled[,9]))
+#Style width
+protest(matrix_effect_size,dist(traits_all_scaled[,10]))
+#Ovary width
+protest(matrix_effect_size,dist(traits_all_scaled[,11]))
+#Ovary length
+protest(matrix_effect_size,dist(traits_all_scaled[,12]))
+#SI index
+protest(matrix_effect_size,dist(traits_all_scaled[,13]))
+
+#Effect size~phylogenetic distance
+protest(matrix_effect_size,evo_distance_its)
+protest(matrix_effect_size,evo_distance_rbcl)
+
+
+
+#SOLANACEAE
+protest(matrix_soly,traits_soly)
+#Selfing rate
+protest(matrix_soly,dist(traits_soly[,1]))
+#Pollen size
+protest(matrix_soly,dist(traits_soly[,2]))
+#Pollen per anther
+protest(matrix_soly,dist(traits_soly[,3]))
+#Ovules
+protest(matrix_soly,dist(traits_soly[,4]))
+#Pollen ovule ratio
+protest(matrix_soly,dist(traits_soly[,5]))
+#Stigmatic area
+protest(matrix_soly,dist(traits_soly[,6]))
+#Stigma length
+protest(matrix_soly,dist(traits_soly[,7]))
+#Stigma width
+protest(matrix_soly,dist(traits_soly[,8]))
+#Style length
+protest(matrix_soly,dist(traits_soly[,9]))
+#Style width
+protest(matrix_soly,dist(traits_soly[,10]))
+#Ovary width
+protest(matrix_soly,dist(traits_soly[,11]))
+#Ovary length
+protest(matrix_soly,dist(traits_soly[,12]))
+#SI index
+protest(matrix_soly,dist(traits_soly[,13]))  
+
+#BRASSICACEAE
+matrix_brra=matrix_effect_size[c(1,2,4,8),c(1,2,4,8)]
+matrix_brra_its=evo_distance_its[c(1,2,4,8),c(1,2,4,8)]
+traits_brra=traits_all_scaled[c(1,2,4,8),]
+
+protest(matrix_brra,traits_brra) 
+protest(matrix_brra,matrix_brra_its) 
+
+#Selfing rate
+protest(matrix_brra,dist(traits_brra[,1]))
+#Pollen size
+protest(matrix_brra,dist(traits_brra[,2]))
+#Pollen per anther
+protest(matrix_brra,dist(traits_brra[,3]))
+#Ovules
+protest(matrix_brra,dist(traits_brra[,4]))
+#Pollen ovule ratio
+protest(matrix_brra,dist(traits_brra[,5]))
+#Stigmatic area
+protest(matrix_brra,dist(traits_brra[,6]))
+#Stigma length
+protest(matrix_brra,dist(traits_brra[,7]))
+#Stigma width
+protest(matrix_brra,dist(traits_brra[,8]))
+#Style length
+protest(matrix_brra,dist(traits_brra[,9]))
+#Style width
+protest(matrix_brra,dist(traits_brra[,10]))
+#Ovary width
+protest(matrix_brra,dist(traits_brra[,11]))
+#Ovary length
+protest(matrix_brra,dist(traits_brra[,12]))
+#SI index
+protest(matrix_brra,dist(traits_brra[,13]))
+
