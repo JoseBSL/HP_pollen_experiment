867360ef3d055fbf034ed90e9386a98f10b885f1
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-09-29 16:47:05 +1000

    t_test ratio, comparing ratios of the three families to give support to the differences that we show in results

---
+++ Scripts/Analysis/t_test_ratios.R
@@ -0,0 +1,26 @@
+#'m going to compare the pollen proportions among families
+#Read data
+d <- read.csv("Data/pollen_ratios.csv")
+
+d_pollen <- subset(d, variable=="non_focal_pollen")
+#length(d_pollen$X)
+#confirming length of the data.frame
+#add_manually column with family
+d_pollen$family_focal <- c("S","B","B","B","S", "S", "S", "B", "C", "B", "B", "B", "C", "B", "C",
+                           "S", "S", "C","S","S")
+
+d_pollen$family_non_focal <- c("C","C","C","C","C","C","C","C","S","S","S","S","S","S","B","B","B",
+                               "B","B","B")
+#select column of interest
+d_pollen <- d_pollen[,c(4,7,8)]
+
+c_pollen <- subset(d_pollen, family_non_focal=="C")
+s_b_pollen <- subset(d_pollen, family_non_focal=="B"|family_non_focal=="S")
+s_pollen <- subset(d_pollen, family_non_focal=="S")
+b_pollen <- subset(d_pollen, family_non_focal=="B")
+
+t.test(c_pollen$ratio,s_b_pollen$ratio)
+#Showing differences between big pollen size and small pollen size families 
+#Convolvulacea compared with solanaceae and brassicaceae
+t.test(s_pollen$ratio,b_pollen$ratio)
+
