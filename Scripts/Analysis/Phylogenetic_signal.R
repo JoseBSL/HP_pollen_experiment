967f5ca14ad54e216e1b75642d7ea394ea84c8d9
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2019-10-03 22:51:03 +1000

    ready for tomorrow

--- Scripts/Analysis/Phylogenetic_signal.R
+++ Scripts/Analysis/Phylogenetic_signal.R
@@ -20,6 +20,7 @@ readRDS("Manuscript_draft/Data/matrix_scale_effect.Rda")
 #From here I start working with the traits
 traits_all <- read.csv("Data/traits_all.csv", sep=",")
 si_index <- readRDS("Data/si_index.RData")
+matrix_scale_effect <- readRDS("Data/matrix_scale_effect.RDa")
 traits_all$si_index <- si_index
 rownames(traits_all) <- rownames(matrix_scale_effect)
 traits_all <- traits_all[,-c(1:3,9,13)]
@@ -144,9 +145,9 @@ si_index <- phylosig(tree=tree_10,x=si_index,method="lambda",test=TRUE)
 #
 tree_10 <- read.newick("Data/no_conv.nwk")
 
-traits_all=traits_all[-c(5,6),]
+traits_all_no_conv=traits_all[-c(5,6),]
 #Phylo signal for selfing rate
-selfing <-as.data.frame(traits_all[,c("Selfing_rate")])
+selfing <-as.data.frame(traits_all_no_conv[,c("Selfing_rate")])
 rownames(selfing) <- tree_10[[3]]
 selfing <- as.matrix((selfing))[,1]
 selfing <- phylosig(tree=tree_10,x=selfing,method="lambda",test=TRUE)
@@ -158,21 +159,21 @@ selfing <- phylosig(tree=tree_10,x=selfing,method="lambda",test=TRUE)
 
 
 #Phylo signal for pollen size
-pollen_size <-as.data.frame(traits_all[,c("pollen_size")])
+pollen_size <-as.data.frame(traits_all_no_conv[,c("pollen_size")])
 rownames(pollen_size) <- tree_10[[3]]
 pollen_size <- as.matrix((pollen_size))[,1]
 pollen_size <- phylosig(tree=tree_10,x=pollen_size,method="lambda",test=TRUE)
 #$`lambda`[1] 0.9971989
 
 #Phylo signal for mean pollen per anther
-mean_pollen_anther <-as.data.frame(traits_all[,c("mean_pollen_anther")])
+mean_pollen_anther <-as.data.frame(traits_all_no_conv[,c("mean_pollen_anther")])
 rownames(mean_pollen_anther) <- tree_10[[3]]
 mean_pollen_anther <- as.matrix((mean_pollen_anther))[,1]
 mean_pollen_anther <- phylosig(tree=tree_10,x=mean_pollen_anther,method="lambda",test=TRUE)
 #$`lambda`[1] 6.610696e-05
 
 #Phylo signal for mean ovules
-mean_ovules <-as.data.frame(traits_all[,c("mean_ovules")])
+mean_ovules <-as.data.frame(traits_all_no_conv[,c("mean_ovules")])
 rownames(mean_ovules) <- tree_10[[3]]
 mean_ovules <- as.matrix((mean_ovules))[,1]
 mean_ovules <- phylosig(tree=tree_10,x=mean_ovules,method="lambda",test=TRUE)
@@ -180,48 +181,48 @@ mean_ovules <- phylosig(tree=tree_10,x=mean_ovules,method="lambda",test=TRUE)
 
 
 #Phylo signal for pollen_ovule_ratio
-pollen_ovule_ratio <-as.data.frame(traits_all[,c("pollen_ovule_ratio")])
+pollen_ovule_ratio <-as.data.frame(traits_all_no_conv[,c("pollen_ovule_ratio")])
 rownames(pollen_ovule_ratio) <- tree_10[[3]]
 pollen_ovule_ratio <- as.matrix((pollen_ovule_ratio))[,1]
 pollen_ovule_ratio <- phylosig(tree=tree_10,x=pollen_ovule_ratio,method="lambda",test=TRUE)
 #$`lambda`[1] 6.610696e-05
 
 #Anthers
-anthers <-as.data.frame(traits_all[,c("anthers")])
+anthers <-as.data.frame(traits_all_no_conv[,c("anthers")])
 rownames(anthers) <- tree_10[[3]]
 anthers <- as.matrix((anthers))[,1]
 anthers <- phylosig(tree=tree_10,x=anthers,method="lambda",test=TRUE)
 
 #Stigma area
-stigma_area <-as.data.frame(traits_all[,c("stigma_area")])
+stigma_area <-as.data.frame(traits_all_no_conv[,c("stigma_area")])
 rownames(stigma_area) <- tree_10[[3]]
 stigma_area <- as.matrix((stigma_area))[,1]
 stigma_area <- phylosig(tree=tree_10,x=stigma_area,method="lambda",test=TRUE)
 #$`lambda`[1] 0.8597021
 
 #Stigma length
-stigma_length <-as.data.frame(traits_all[,c("stigma_length")])
+stigma_length <-as.data.frame(traits_all_no_conv[,c("stigma_length")])
 rownames(stigma_length) <- tree_10[[3]]
 stigma_length <- as.matrix((stigma_length))[,1]
 stigma_length <- phylosig(tree=tree_10,x=stigma_length,method="lambda",test=TRUE)
 #$`lambda`[1] 0.6961634
 
 #Stigma_width
-stigma_width <-as.data.frame(traits_all[,c("stigma_width")])
+stigma_width <-as.data.frame(traits_all_no_conv[,c("stigma_width")])
 rownames(stigma_width) <- tree_10[[3]]
 stigma_width <- as.matrix((stigma_width))[,1]
 stigma_width <- phylosig(tree=tree_10,x=stigma_width,method="lambda",test=TRUE)
 #$`lambda`[1] 0.7292784
 
 #Stigma_surface
-stigma_surface <-as.data.frame(traits_all[,c("stigma_surface")])
+stigma_surface <-as.data.frame(traits_all_no_conv[,c("stigma_surface")])
 rownames(stigma_surface) <- tree_10[[3]]
 stigma_surface <- as.matrix((stigma_surface))[,1]
 stigma_surface <- phylosig(tree=tree_10,x=stigma_surface,method="lambda",test=TRUE)
 #$`lambda`[1] 0.7292784
 
 #Phylo signal style length
-style_length <-as.data.frame(traits_all[,c("style_length")])
+style_length <-as.data.frame(traits_all_no_conv[,c("style_length")])
 rownames(style_length) <- tree_10[[3]]
 style_length <- as.matrix((style_length))[,1]
 style_length <- phylosig(tree=tree_10,x=style_length,method="lambda",test=TRUE)
@@ -229,7 +230,7 @@ style_length <- phylosig(tree=tree_10,x=style_length,method="lambda",test=TRUE)
 
 
 #Phylo signal style width
-style_width <-as.data.frame(traits_all[,c("style_width")])
+style_width <-as.data.frame(traits_all_no_conv[,c("style_width")])
 rownames(style_width) <- tree_10[[3]]
 style_width <- as.matrix((style_width))[,1]
 style_width <- phylosig(tree=tree_10,x=style_width,method="lambda",test=TRUE)
@@ -237,21 +238,21 @@ style_width <- phylosig(tree=tree_10,x=style_width,method="lambda",test=TRUE)
 
 
 #Phylo signal ovary width
-ovary_width <-as.data.frame(traits_all[,c("ovary_width")])
+ovary_width <-as.data.frame(traits_all_no_conv[,c("ovary_width")])
 rownames(ovary_width) <- tree_10[[3]]
 ovary_width <- as.matrix((ovary_width))[,1]
 ovary_width <- phylosig(tree=tree_10,x=ovary_width,method="lambda",test=TRUE)
 #$`lambda`[1] 6.610696e-05
 
 #Phylo signal ovary length
-ovary_length <-as.data.frame(traits_all[,c("ovary_length")])
+ovary_length <-as.data.frame(traits_all_no_conv[,c("ovary_length")])
 rownames(ovary_length) <- tree_10[[3]]
 ovary_length <- as.matrix((ovary_length))[,1]
 ovary_length <- phylosig(tree=tree_10,x=ovary_length,method="lambda",test=TRUE)
 #$`lambda`[1] 0.4881212
 
 #si_index
-si_index <-as.data.frame(traits_all[,c("si_index")])
+si_index <-as.data.frame(traits_all_no_conv[,c("si_index")])
 rownames(si_index) <- tree_10[[3]]
 si_index <- as.matrix((si_index))[,1]
 si_index <- phylosig(tree=tree_10,x=si_index,method="lambda",test=TRUE)
@@ -265,9 +266,9 @@ effect_size_all$species <- c("SOME","SOLY","PEIN", "CAAN",
                                "IPPU", "IPAQ", "SIAL", "ERSA",
                                "BRRA", "BROL")
 effect_size_all=effect_size_all[,-c(1,3,4)]
-traits_all$species <- rownames(traits_all)
-traits_all <- merge(effect_size_all, traits_all, by="species" )
-Cohen_d <-as.data.frame(traits_all[,c("Cohen_d")])
+traits_all_no_conv$species <- rownames(traits_all_no_conv)
+traits_all_no_conv <- merge(effect_size_all, traits_all_no_conv, by="species" )
+Cohen_d <-as.data.frame(traits_all_no_conv[,c("Cohen_d")])
 rownames(Cohen_d) <- tree_10[[3]]
 Cohen_d <- as.matrix((Cohen_d))[,1]
 Cohen_d <- phylosig(tree=tree_10,x=Cohen_d,method="lambda",test=TRUE)
@@ -276,7 +277,8 @@ Cohen_d <- phylosig(tree=tree_10,x=Cohen_d,method="lambda",test=TRUE)
 
 #Now no Solanaceae
 tree_no_sol <- read.newick("Data/no_sol.nwk")
-
+traits_all_no_sol <- traits_all[,-c(3,7,9,10)]
 #Now no Brassicaceae
 
 tree_no_sol <- read.newick("Data/no_bra.nwk")
+traits_all_no_bra <- traits_all[,-c(1,2,4,8)]
